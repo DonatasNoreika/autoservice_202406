1468665d96941beb16528a1a0a41a360ea9dfd8f
DonatasNoreika <donoras@gmail.com>
2024-06-11 08:39:27 +0300

    5: +Puslapyje įdėti paieškos laukelį, kuris ieškotų automobilių pagal savininką, modelį, valstybinį numerį, VIN kodą

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from .views import (index,
                     cars,
                     car,
+                    search,
                     OrderListView,
                     OrderDetailView)
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path("cars/<int:car_id>", car, name="car"),
     path("orders/", OrderListView.as_view(), name="orders"),
     path("orders/<int:pk>", OrderDetailView.as_view(), name="order"),
+    path('search/', search, name="search"),
 ]
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -3,6 +3,7 @@ from .models import Service, Order, Car
 from django.views.generic import (ListView,
                                   DetailView)
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -45,3 +46,13 @@ class OrderDetailView(DetailView):
     model = Order
     template_name = "order.html"
     context_object_name = "order"
+
+
+def search(request):
+    query = request.GET.get('query')
+    cars = Car.objects.filter(Q(client_name__icontains=query) | Q(car_model__make__icontains=query) | Q(car_model__model__icontains=query) | Q(license_plate__icontains=query) | Q(vin_code__icontains=query))
+    context = {
+        "cars": cars,
+        "query": query,
+    }
+    return render(request, template_name='search.html', context=context)
